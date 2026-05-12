efbad19a830936cc7dd6f6a9082255d52b9fc87a
Sinon <34333654+Catta1997@users.noreply.github.com>
2026-05-12 14:56:42 +0200

    fix/MangaDot): icon fix

--- src/MangaDot/pbconfig.ts
+++ src/MangaDot/pbconfig.ts
@@ -3,8 +3,8 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "MangaDot",
   description: "Extension that pulls content from MangaDot.net",
-  version: "1.0.0-alpha.2",
-  icon: "icon.svg",
+  version: "1.0.0-alpha.3",
+  icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
   capabilities: [
